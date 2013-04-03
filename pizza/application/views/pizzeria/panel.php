38a64e368cc15a625af4a7ce24a30d3cfde3b4d4
Karol Sliwinski <karol.sliwinski@yahoo.pl>
2013-04-03 18:12:12 +0200

    Revert "polskie znaki (poprawka)"
    This reverts commit ae79cc4262a80b9b3da4e970d59418a47d621023.

--- pizza/application/views/pizzeria/panel.php
+++ pizza/application/views/pizzeria/panel.php
@@ -1,9 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3c.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3c.org/1999/xhtml" xml:lang="pl" lang="pl">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 
-<meta http-equiv="content-type" content="text/html; charset=iso-8859-2" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="<?PHP echo base_url(); ?>application/views/pizzeria/pom.css" />
 
 <title>Pizzeria</title>
@@ -15,7 +14,7 @@
 <div id="header"><br />
 
 <h3>PKKD</h3>
-<p><strong>"Pizza najlepsza w mieście "</strong><br />
+<p><strong>"Pizza najlepsza w mieĹcie "</strong><br />
  </p>
 </div>
 
@@ -30,7 +29,7 @@
 <div id="avmenu">
 <h2 class="hide">Menu:</h2>
 <ul>
-<li><a href="<?php echo site_url('pizza/index');?>">Strona Główna</a></li>
+<li><a href="<?php echo site_url('pizza/index');?>">Witamy</a></li>
 
 
 
